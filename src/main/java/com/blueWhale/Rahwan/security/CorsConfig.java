93fa11436fe456d926f52dcf886aed6b686484fd
mohammed-elhlawany <mohamedelhlawany90@gmail.com>
2026-01-07 16:16:11 +0200

    change database password

--- src/main/java/com/blueWhale/Rahwan/security/CorsConfig.java
+++ src/main/java/com/blueWhale/Rahwan/security/CorsConfig.java
@@ -1,23 +1,19 @@
 //package com.blueWhale.Rahwan.security;
 //
-//import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
 //@Configuration
-//public class CorsConfig {
+//public class CorsConfig implements WebMvcConfigurer {
 //
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/api/**")
-//                        .allowedOrigins("*") // أو domain معين
-//                        .allowedMethods("GET","POST","PUT","DELETE","PATCH")
-//                        .allowCredentials(true);
-//            }
-//        };
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(false)
+//                .maxAge(3600);
 //    }
 //}

90d688c6fc863abeea86a54d7ae457bb2e105ed6
Mohamed Elhlawany <mohamedelhlawany90.com>
2026-05-12 15:08:43 +0300

    add spring boot dependancy

--- src/main/java/com/blueWhale/Rahwan/location/Location.java
+++ src/main/java/com/blueWhale/Rahwan/location/Location.java
@@ -1,14 +1,9 @@
 package com.blueWhale.Rahwan.location;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -29,7 +24,3 @@ public class Location {
     @Enumerated(EnumType.STRING)
     private LocationType type;
 }
-
-
-
-
